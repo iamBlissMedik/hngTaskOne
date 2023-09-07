c5e9cc40878246df90983047faee7dd331fe2d5f
Ellams Bliss <ellamsbliss@gmail.com>
2023-09-07 20:56:23 +1000

    first task

---
+++ app.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const slackUserName = document.querySelector('[data-textid="slackUserName"]');
+  const currentDayOfTheWeek = document.querySelector(
+    '[data-textid="currentDayOfTheWeek"]'
+  );
+  const currentUTCTime = document.querySelector(
+    '[data-textid="currentUTCTime"]'
+  );
+
+  //  Slack Name
+  slackUserName.textContent = "Blizz";
+
+  //  Current Day of the Week
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDate = new Date();
+  const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+  currentDayOfTheWeek.textContent = dayOfWeek;
+
+  // Current UTC Time
+  const options = { timeZone: "UTC", hour12: false };
+  currentUTCTime.textContent = new Date().toLocaleTimeString(
+    undefined,
+    options
+  );
+});
